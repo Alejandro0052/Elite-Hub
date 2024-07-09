57f242d74b865961d92453cea4ac33125002f8e7
Alejandro <mariaeugenialopez456@mail.com>
2024-07-09 07:45:31 -0500

    Agregando las tablas Invitados y Pqr

--- Elite/models.py
+++ Elite/models.py
@@ -34,8 +34,30 @@ class Patrocinadores(models.Model):
     
     
 class Marcas(models.Model):
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE )
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    descripcion = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    numero_telefono = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.nombre} - {self.descripcion}'
 
 
+class Invitados(models.Model):
+  # usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE, primary_key=True)
+    descripcion = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    numero_telefono = models.IntegerField()
 
+    def __str__(self):
+        return f'{self.nombre} - {self.descripcion}'
+
+
+class Pqrs(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    asunto = models.CharField(max_length=30)
+    descripcion = models.CharField(max_length=250)
 
